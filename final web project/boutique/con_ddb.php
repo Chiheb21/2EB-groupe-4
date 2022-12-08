a4ef98a4ccf565ee8600e9d0793e2c548bccf9b5
yamdi <99355249+yamdi@users.noreply.github.com>
2022-12-08 23:23:57 +0100

    Delete final web project directory

--- final web project/boutique/con_ddb.php
+++
@@ -1,9 +0,0 @@
-<?php 
-//connexion à la base de données
-$con = mysqli_connect("localhost","root","","panier");
-//verifier la connexion
-if(!$con) die('Erreur : '.mysqli_connect_error());
-
-
-
-?>
